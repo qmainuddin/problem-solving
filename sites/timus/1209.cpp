7db4604c2464556580efa22b66869203a5252868
mainuddin.sazal <mainuddin.sazal@gmail.com>
2018-12-14 22:42:17 +0600

    solved 1209 in timus

--- sites/timus/1209.cpp
+++ sites/timus/1209.cpp
@@ -2,39 +2,18 @@
 
 using namespace std;
 
-bool isPerfectSquare(long long number)
+int get_digit(unsigned long long digit_position)
 {
-    long long iVar;
-    float fVar;
-
-    fVar=sqrt((double)number);
-    iVar=fVar;
-
-    if(iVar==fVar)
-        return true;
-    else
-        return false;
-}
-
-int get_digit(long long digit_position)
-{
-    /*if(digit_position == 0) return 1;
-    for(long long i=1; ; i++)
-    {
-        long long digit_count = (i*(i+1))/2;
-        if(digit_count == digit_position) return 1;
-        else if(digit_count > digit_position) return 0;
-    }*/
-
-    long long result = 8*(digit_position) -7;
-    if(isPerfectSquare(result)) return 1;
+    double result = (sqrt((8*digit_position)-7));
+    if(result == (unsigned long long)result) return 1;
     return 0;
 }
+
 int main()
 {
     int n;
     cin >> n;
-    long long k[n];
+    unsigned long long k[n];
     for(int i =0; i<n; i++)
     {
         cin >> k[i];
