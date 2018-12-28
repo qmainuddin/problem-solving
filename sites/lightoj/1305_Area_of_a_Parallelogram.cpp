8e3f9d374df04a15fb4a831e4ef14a0f886b188e
mainuddin.sazal <mainuddin.sazal@gmail.com>
2018-12-28 21:57:09 +0600

    added lightoj solution 1305

--- sites/lightoj/1305_Area_of_a_Parallelogram.cpp
+++ sites/lightoj/1305_Area_of_a_Parallelogram.cpp
@@ -17,21 +17,9 @@ int main()
         ll Ax,Ay, Bx, By, Cx, Cy, Dx, Dy;
         cin >> Ax >> Ay >> Bx >> By >> Cx >> Cy;
         cout << "Case " << i << ": ";
-        ll p = (Ay*Cx) - (By*Cx) + (Bx*Cy) - (Ax*Cy);
-        ll q = Ay-2*By+Cy;
-        ll l = By*(Ax+Cx)*(Ay-By);
-        ll o = Bx * (Ay+Cy) * (Ay-By);
-        ll m = ((Bx-Ax) * (Ay-2*By+Cy)) - ((Ay-By) *(2*By-Ax-Cx));
-        Dy = ((p*q) + l -o)/m;
-        Dx = (Bx*(Ay+Cy)-By*(Ax+Cx)-Dy*(2*Bx-Ax-Cx))/q;
-
-        ll base = sqrt(((Bx-Ax) * (Bx-Ax)) + ((By-Ay) * (By-Ay)));
-        ll theta = ((Ay-Dy) * (Bx-Ax)) - ((Ay-By) * (Dx-Ax));
-        theta = atan2(theta, ((Ay-By) * (Ay-Dy)) - ((Bx-Ax)*(Dx-Ax)));
-        cout << "Theta : " << theta << endl;
-        ll height = sqrt(((Dx-Ax) * (Dx-Ax)) + ((Dy-Ay) * (Dy-Ay))) * abs(sin(theta));
-
-        ll area = height * base;
+        Dx = Ax + (Cx-Bx);
+        Dy = Ay + (Cy-By);
+        ll area = abs((Ax*By) + (Bx*Dy) + (Dx*Ay) - ((Ax*Dy) + (Dx*By) + (Bx*Ay)));
         cout << Dx << " " << Dy << " " << area << endl;
     }
     return 0;
