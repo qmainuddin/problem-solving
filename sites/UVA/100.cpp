68f0b82115e94c814372bded6d13cb44e3e6f7a1
mainuddin_sazal <mainuddin.sazal@gmail.com>
2019-01-01 21:32:11 +0600

    added uva solution, not accepted yet

--- sites/UVA/100.cpp
+++ sites/UVA/100.cpp
@@ -1,45 +1,43 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+typedef long long int ll;
 
 int counters[1000000], loCount;
 
-int getValue(int n)
+ll getValue(ll n)
 {
-    if(counters[n] != 0)
-    {
-        cout << "Value: "  << counters[n] << endl;
-        return counters[n];
-    }
+    loCount++;
     if(n<=1)
     {
-        counters[1] = 1;
         return 1;
     }
-    if((n&1) == 0) counters[n] = getValue(n>>2)+1;
-    else counters[n] = getValue(3*n +1) + 1;
-    cout << "n: " << n << " counters[" << n<< "] : " << counters[n] << endl;
-    return counters[n];
+    if(n%2 == 0)
+    {
+        return getValue(n/2);
+    }
+    else return getValue(3*n +1);
 }
 
 int main()
 {
-    //freopen("in.txt", "r", stdin);
-    //freopen("out.txt", "w", stdout);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 
-    int a,b;
+    ll a,b;
     memset(counters, 0, sizeof(counters));
     counters[1] = 1;
-    cout << counters[2] << endl << endl;
+    counters[2] = 2;
     while(cin >> a >> b)
     {
         int max = 0;
-        for(int i=a; i<=b; i++)
+        for(ll i=a; i<=b; i++)
         {
-            int countVal = getValue(i);
-            if(countVal > max)
+            loCount = 0;
+            getValue(i);
+            if(loCount > max)
             {
-                max = countVal;
+                max = loCount;
             }
         }
         cout << a << " " << b << " " << max << endl;
