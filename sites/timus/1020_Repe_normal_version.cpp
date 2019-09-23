bbecdaaac94fadfa005a884d3f5994dda8fdbf14
qmainuddin <mainuddin.sazal@gmail.com>
2019-09-23 22:18:49 +0600

    added timus 1020-rope

---
+++ sites/timus/1020_Repe_normal_version.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    double r, cordinates[n][2];
+
+    cin >> n >> r;
+
+    for(int i=0; i<n; i++)
+    {
+        cordinates[i][]
+    }
+
+}
